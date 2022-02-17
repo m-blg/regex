e6ad57b96f556fa1cb577ce24ec59ddf27b1d432
mbgl <caaalm3@gmail.com>
2022-02-18 02:27:44 +0300

    parse_regex - working, match - not. Commit before rewriting 'match' function.

--- Main.cc
+++ Main.cc
@@ -1,39 +1,48 @@
 
+#include "cp_lib/buffer.cc"
 #include "cp_lib/string.cc"
 #include "cp_lib/io.cc"
+#include "cp_lib/basic.cc"
+#include <cassert>
+#include <cstdio>
+#include <cstring>
 
 using namespace cp;
 
 
-struct Graph {
+struct Regex_FA {
     struct Transition {
         i32 node_index;
-        str s;
+        dstrb s;
         i32 minq;
         i32 maxq;
+        i32 q;
     };
     darr<darr<Transition>> transitions;
 
     darr<Transition>& operator[](u32 node_i);
 };
 
-darr<Graph::Transition>& Graph::operator[](u32 node_i) {
+darr<Regex_FA::Transition>& Regex_FA::operator[](u32 node_i) {
     return transitions[node_i];
 }
 
-void print(Graph::Transition t) {
+void print(Regex_FA::Transition t) {
     print("(", t.node_index,",", t.s, ")");
 }
 
-dstrb to_graphvis(Graph g) {
+dstrb to_graphvis(Regex_FA g) {
     dstrb sb; init(&sb, 30 * len(g.transitions));
 
     sprint_fmt(&sb, "digraph G {");
     for (u32 i = 0; i < len(g.transitions); i++) {
-        for (u32 j = 0; j < len(g[i]); j++) {            
-            //sprint(&sb, i, " -> ", g[i][j].node_index, "[label=\"", g[i][j].s, "\"];\n");
-            sprint_fmt(&sb, "\t%d -> %d [label=\"%.*s\"];\n", 
-                    i, g[i][j].node_index, g[i][j].s.cap, g[i][j].s.buffer);
+        for (u32 j = 0; j < len(g[i]); j++) {
+            str s = to_str(g[i][j].s);
+            if (is_empty(s))
+                s = str{"\\\\e"};
+            sprint_fmt(&sb, "\t%d -> %d [label=\"%.*s, %d, %d\"];\n", 
+                    i, g[i][j].node_index, len(s), s.buffer, 
+                    g[i][j].minq, g[i][j].maxq);
         }
     }
     sprint_fmt(&sb, "}");
@@ -41,26 +50,26 @@ dstrb to_graphvis(Graph g) {
     return sb;
 }
 
-//init :: Graph -> darr<darr<Transition>> -> void
+//init :: Regex_FA -> darr<darr<Transition>> -> void
 
-void init(Graph *self) {
+void init(Regex_FA *self) {
     init(&self->transitions);
-    // apply((void(*)(darr<Graph::Transition>*, u32)) &init, self->transitions);
+    // apply((void(*)(darr<Regex_FA::Transition>*, u32)) &init, self->transitions);
 }
-void init(Graph *self, darr<darr<Graph::Transition>> tr) { 
+void init(Regex_FA *self, darr<darr<Regex_FA::Transition>> tr) { 
     self->transitions = tr; 
 }
 
-//void shut(Graph *self) {
-    //applyp((void(*)(darr<Graph::Transition>*)) &shut, self->transitions);
+//void shut(Regex_FA *self) {
+    //applyp((void(*)(darr<Regex_FA::Transition>*)) &shut, self->transitions);
     //shut(self);
 //}
-void shut(Graph *self) {
+void shut(Regex_FA *self) {
     // applyp([](auto x) {shut(x);}, self->transitions);
     apply([](auto x) {shut(x);}, self->transitions);
     shut(&self->transitions);
 }
-u32 add_node(Graph *self, u32 count=1) {
+u32 add_node(Regex_FA *self, u32 count=1) {
     u32 si = len(self->transitions);
     for (u32 i = 0; i < count; i++) {
         push(&self->transitions, {});
@@ -69,7 +78,7 @@ u32 add_node(Graph *self, u32 count=1) {
     return si;
 }
 
-void add_transition(Graph *self, i32 node, Graph::Transition tr) {
+void add_transition(Regex_FA *self, i32 node, Regex_FA::Transition tr) {
     push(&(*self)[node], tr);
 }
 
@@ -79,26 +88,130 @@ void mabort(const char* msg) {
     exit(-1);
 }
 
-auto re_reserved = pack('?', '*', '+', '(', ')', '{', '}', '|', '\\');
+auto re_reserved = pack('?', '*', '+', '(', ')', '{', '}', '|');
 auto re_reserved_group = pack('(', ')', '|');
+auto re_reserved_quantifier = pack('?', '*', '+', '{');
 
 
-Graph parse_regex(str re) {
-    Graph g; init(&g);
+char
+parse_letter(str *re) {
+    if ((*re)[0] == '\\') {
+        if (len(*re) == 1)
+            mabort("bad regex");
+        char l = (*re)[1];
+        *re = drop(2, *re);
+        return l;
+    }
+    char l = (*re)[0];
+    *re = drop(1, *re);
+    return l;
+}
+
+// doesn't include the letter before a quantifier
+dstrb
+parse_string(str *re) {
+    //if (is_empty(*re) || is_elem((*re)[0], re_reserved))
+        //return {};
+
+    dstrb s; init(&s, cap(*re));
+    while(!is_empty(*re)) {
+
+        if (is_elem((*re)[0], re_reserved)) {
+            if (!is_elem((*re)[0], re_reserved_group)) {
+                s.len--;
+                re->buffer--;
+                re->cap++;
+            }
+            break;
+        }
+        char l = parse_letter(re);
+        push(&s, l);
+    }
+
+    return s;
+}
+
+void
+parse_quantifier(str *re, i32 *minq, i32 *maxq) {
+    switch (head(*re)) {
+    case '?': {
+        *re = drop(1, *re);
+        *minq = 0; *maxq = 1;
+    } break;
+
+    case '*': {
+        *re = drop(1, *re);
+        *minq = 0; *maxq = INT_MAX;
+    } break;
+
+    case '+': {
+        *re = drop(1, *re);
+        *minq = 1; *maxq = INT_MAX;
+    } break;
+
+    case '{': {
+        const char* p = strchr(re->buffer, '}');
+        if (p == null)
+            mabort("Bad regex");
+        i32 l = p - re->buffer + 1;
+                
+        if (sscanf(re->buffer, "{%d, %d}", minq, maxq) != 2)
+            mabort("Bad regex");
+        *re = drop(l, *re);
+    } break;
+
+    default: {
+         mabort("Bad regex");
+    } break;
+    }
+}
+
+dstrb
+parse_letter_with_quantifier(str *re, i32 *minq, i32 *maxq) {
+    dstrb s; init(&s, 1);
+    push(&s, parse_letter(re));
+    parse_quantifier(re, minq, maxq);
+    return s;
+}
+
+void
+parse_group_quantifier(str *re, i32 *minq, i32 *maxq) {
+    *re = drop(1, *re);
+    i32 bq = 1;
+    while (bq > 0 && !is_empty(*re)) {
+        if (head(*re) == '(') {    bq++; } 
+        else if (head(*re) == ')') bq--;
+        *re = drop(1, *re);
+    }
+    if (bq != 0)
+        mabort("Bad regex");
+
+    if (is_empty(*re) || !is_elem(head(*re), re_reserved_quantifier)) {
+        *minq = 1, *maxq = 1;
+        return;
+    }
+    parse_quantifier(re, minq, maxq);
+}
+
+Regex_FA
+parse_regex(str re)
+{
+    Regex_FA g; init(&g);
 
     struct S {
         i32 i, si, fi;
+        str re;
     };
     darr<S> stack; init(&stack, 1);
 
     add_node(&g, 2);
-    push(&stack, {0, 0, 1});
+    push(&stack, {0, 0, 1, re});
     while(!is_empty(stack)) {
         S& cur = back(stack);
         // cur.i, cur.si, cur.fi
 
-        if (is_empty(re)) {
-            add_transition(&g, cur.i, {cur.fi, "", 1, 1});
+        if (is_empty(cur.re)) {
+            add_transition(&g, cur.i, {cur.fi, dstrb_from(""), -1, 1});
             pop(&stack);
             if (!is_empty(stack))
                 mabort("Bad regex");
@@ -106,157 +219,81 @@ Graph parse_regex(str re) {
             return g;
         }
 
-        // try process string
-        if ( !is_elem(head(re), re_reserved) && 
-             (len(re) == 1 || !is_elem(re[1], re_reserved) || 
-             is_elem(re[1], re_reserved_group)) ) 
-        {
-            str s = {re.buffer, 0};
-            while (true) {
-                i32 c_ind = len(s);
-                if (c_ind == len(re)-1) {
-                    s.cap++;
-                    // u32 node_i = add_node(&g);
-                    // add_transition(&g, cur.i, {node_i, s});
-                    // add_transition(&g, node_i, {cur.fi, ""});
-                    add_transition(&g, cur.i, {cur.fi, s, 1, 1});
-                    pop(&stack);
-                    if (!is_empty(stack))
-                        mabort("Bad regex");
-                    return g;
-                }
-
-                if (is_elem(re[c_ind+1], re_reserved)) {
-                    if (is_elem(re[c_ind+1], re_reserved_group)) {
-                        s.cap++;
-                    }
-
-                    if (re[c_ind+1] == '|') {
-                        add_transition(&g, cur.i, {(i32)cur.fi, s, 1, 1});
-                        cur.i = cur.si;
-                        re = drop(len(s)+1, re);
-                    } else {
-                        u32 node_i = add_node(&g);
-                        add_transition(&g, cur.i, {(i32)node_i, s, 1, 1});
-                        cur.i = node_i;
-                        re = drop(len(s), re);
-                    }
-                    break;
-                }
-
-                s.cap++;
+        if (!is_elem(cur.re[0], re_reserved)) {
+            dstrb s = parse_string(&cur.re);
+            if (!is_empty(s)) {
+                //if (!is_empty(re) && (re[0] == '|' || re[0] == ')'))
+                i32 node_i = (i32)add_node(&g);
+                add_transition(&g, cur.i, {node_i, s, -1, 1});
+                cur.i = node_i;
+                continue;
             }
+
+            i32 minq, maxq;
+            dstrb l = parse_letter_with_quantifier(&cur.re, &minq, &maxq);
+            i32 node_i = (i32)add_node(&g);
+            add_transition(&g, cur.i, {node_i, dstrb_from(""), -1, 1});
+            add_transition(&g, node_i, {node_i, l, minq, maxq});
+            cur.i = node_i;
             continue;
         }
 
-        // the first sym is not reserved, but the second one is
-        if ( !is_elem(head(re), re_reserved) ) {
-            switch (re[1])
-            {
-            case '?':
-                add_transition(&g, cur.i, {cur.i, str{&head(re), 1}, 0, 1});
-                // push(&stack, cur);
-                re = drop(2, re);
-                break;
-            case '*':
-                add_transition(&g, cur.i, {cur.i, str{&head(re), 1}, 0, INT_MAX});
-                // push(&stack, cur);
-                re = drop(2, re);
-                break;
-            case '+':
-                add_transition(&g, cur.i, {cur.i, str{&head(re), 1}, 1, INT_MAX});
-                // push(&stack, cur);
-                re = drop(2, re);
-                break;
-            case '{':
-                // parse {minq, maxq}
-                assert(false);
-                break;
-
-            // '|', '(', ')', or error         
-            default:
-                u32 node_i = add_node(&g);
-                add_transition(&g, cur.i, {(i32)node_i, str{&head(re), 1}, 1, 1});
-                // push(&stack, {node_i, cur.si, cur.fi});
-                cur.i = node_i;
-                re = drop(1, re);
-                break;
-            }
-            continue;           
-        }
-
-        switch (head(re))
+        switch (head(cur.re)) 
         {
         case '(': {
-            i32 nfi = (i32)add_node(&g, 1);
-            // add_transition(&g, cur.i, {nsi, "", 1, 1});
-            // push(&stack, cur);
-            push(&stack, {cur.i, cur.i, nfi});
-            re = drop(1, re);
+            i32 minq, maxq;
+            str temp_re = drop(1, cur.re);
+            parse_group_quantifier(&cur.re, &minq, &maxq);
+
+            i32 loop_node_i = (i32)add_node(&g, 2);
+            i32 sni = loop_node_i + 1;
+            add_transition(&g, cur.i, {loop_node_i, dstrb_from(""), -1, 1}); // from cur to loop node
+            add_transition(&g, loop_node_i, {sni, dstrb_from(""), minq, maxq}); // from loop node to group start node
+            cur.i = loop_node_i;
+            push(&stack, {sni, sni, loop_node_i, temp_re});
         } break;
 
         case ')': {
-            add_transition(&g, cur.i, {cur.fi, "", 1, 1});
-            if (len(re) > 1) {
-                switch (re[1])
-                {
-                case '?':
-                    add_transition(&g, cur.fi, {cur.si, "", 0, 1});
-                    // push(&stack, cur);
-                    re = drop(2, re);
-                    break;
-                case '*':
-                    add_transition(&g, cur.fi, {cur.si, "", 0, INT_MAX});
-                    // push(&stack, cur);
-                    re = drop(2, re);
-                    break;
-                case '+':
-                    add_transition(&g, cur.fi, {cur.si, "", 1, INT_MAX});
-                    // push(&stack, cur);
-                    re = drop(2, re);
-                    break;
-                case '{':
-                    // parse {minq, maxq}
-                    assert(false);
-                    break;
-
-                // '|', '(', ')', or error         
-                default:
-                    re = drop(1, re);
-                    break;
-                }
-            }
-            if (len(stack) > 1)
-                stack[len(stack)-2].i = cur.si;
-            pop(&stack); // after pop cur (S&) is invalid
+            add_transition(&g, cur.i, {cur.fi, dstrb_from(""), -1, 1});
+            pop(&stack);
         } break;
-        
-        case '|':
-            // mabort("Bad regex");
-            add_transition(&g, cur.i, {cur.fi, "", 1, 1});
+
+        case '|': {
+            add_transition(&g, cur.i, {cur.fi, dstrb_from(""), -1, 1});
             cur.i = cur.si;
-            re = drop(1, re);
-            break;
-            
-        
-        default:
+            cur.re = drop(1, cur.re);
+        } break;
+
+        default: {
             mabort("Bad regex");
-            break;
+        } break;
         }
+
     }
 
-    if (!is_empty(re)) {
-        // error
-    }
+    mabort("Bad regex");
     return g;
 }
 
-void test() {
-    Graph g;
+bool
+are_transitions_correct(Regex_FA re) {
+    for (auto it = begin(re.transitions); it != end(re.transitions); it++) {
+        for (auto it2 = begin(*it); it2 != end(*it); it2++) {
+            if (it2 != begin(*it) && it2->minq > 0)
+                return false;
+        }
+    }
+
+    return true;
+}
+
+void
+test1() {
+    Regex_FA g;
     init(&g);
     add_node(&g, 3);
-    add_transition(&g, 0, {1, "a"});
-    add_transition(&g, 1, {2, "b"});
+    add_transition(&g, 0, {1, dstrb_from("a")});
+    add_transition(&g, 1, {2, dstrb_from("b")});
 
     // *begin(*begin(g.transitions));
     print(g.transitions, '\n');
@@ -265,16 +302,120 @@ void test() {
     shut(&g);
 }
 
-int main() {
-    // Graph g = parse_regex("ab(c|b*)*d|aaa|aa*");
-    Graph g = parse_regex("(c|b*)*");
+void
+test_regex(str re, str of_name_no_ext) {
+    Regex_FA g = parse_regex(re);
+    printf("Are transitions correct: %d\n", are_transitions_correct(g));
+
     // print(g.transitions);
     dstrb gvis = to_graphvis(g);
-    write(to_str(gvis), "fa.dot");
-    system("dot -Tpng fa.dot -o fa.png");
+
+    dstrb of_dot; init(&of_dot, len(of_name_no_ext) + 4);
+    dstrb of_png; init(&of_png, len(of_name_no_ext) + 4);
+    cat(&of_dot, pack(of_name_no_ext, str{".dot\0"}));
+    cat(&of_png, pack(of_name_no_ext, str{".png\0"}));
+
+    write(to_str(gvis), of_dot.buffer);
+    dstrb cmd; init(&cmd, 30);
+    sprint_fmt(&cmd, "dot -Tpng %s -o %s\0", of_dot.buffer, of_png.buffer);
+    system(cmd.buffer);
+    clear(&cmd); sprint_fmt(&cmd, "rm %s\0", of_dot.buffer);
+    system(cmd.buffer);
+
     shut(&gvis);
+    shut(&of_dot);
+    shut(&of_png);
+    shut(&cmd);
 
     shut(&g);
+}
 
-    fprintf(stdout, "test\n");
+
+// NOTE: we need explicit loop flags (let's use q field of Transition)
+
+bool
+match(Regex_FA re, str s)
+{
+    struct S {
+        i32 state_i, tran_i;
+        str s;
+    };
+    darr<S> stack; init(&stack, 1);
+
+    push(&stack, {0, 0, s});
+
+    while (!is_empty(stack)) {
+        S cur = back(stack);
+        pop(&stack);
+
+        if (cur.state_i == 1) { // finish state
+            return is_empty(cur.s);
+        } 
+        
+        assert(is_bounded(cur.state_i, 0, (i32)len(re.transitions) - 1));
+        assert(is_bounded(cur.tran_i, 0, (i32)len(re.transitions[cur.state_i]) - 1));
+        // if (!is_bounded(cur.tran_i, 0, len(re.transitions[cur.state_i]) - 1)) {
+        //     pop(&stack);
+        //     continue;
+        // }
+        Regex_FA::Transition& tran = re.transitions[cur.state_i][cur.tran_i];
+        if (tran.q >= tran.maxq) { // end of the loop
+            tran.q = 0;
+            cur.tran_i++;
+            push(&stack, cur);
+            continue;
+        }
+        if (tran.q >= tran.minq && cur.tran_i < len(re.transitions[cur.state_i]) - 1) {
+            push(&stack, {cur.state_i, cur.tran_i+1, cur.s});
+        }
+        
+        if (to_str(tran.s) != take(len(tran.s), cur.s)) {
+            if (cur.tran_i == len(re.transitions[cur.state_i]) - 1 ||
+                tran.q < tran.minq)
+            {
+                // fail: whole state, backtrack
+                
+                if (is_empty(stack))
+                    return false;
+                S& frame = back(stack);
+                re[frame.state_i][0].q = 0; // reset counter for loop (if present)
+                continue;
+            }
+                
+            if (cur.tran_i == 0) // if loop (or not)
+                re[cur.state_i][0].q = 0; 
+            cur.tran_i++;
+
+            push(&stack, cur);
+            continue;
+        }
+
+        cur.s = drop(len(tran.s), cur.s);
+        if (cur.tran_i == 0 && tran.minq != -1) // if loop
+            tran.q++;
+        cur.state_i = tran.node_index;  
+        cur.tran_i = 0;
+        push(&stack, cur);
+    }
+
+    assert(false);
+    return false;
+}
+
+
+void
+test_match(str re, str s) {
+    Regex_FA re_fa = parse_regex(re);
+    printf("Result: %d\n", match(re_fa, s));
+}    
+
+int main() {
+    // test_regex("(c|b){3,4}", "test/test1");
+    // test_regex("((o|b)+|c{3, 4}){9, 11}", "test/test2");
+    // test_match("ab+a", "abbbbbbbbbbba");
+    // test_regex("a(b|c)a", "test/test3");
+    // test_match("a(b|c)a", "aba");
+    // test_regex("a(b|c)*a", "test/test4");
+    test_match("a(b|c)*a", "abcba");
+    // test_match("(b|c)", "c");
 }
